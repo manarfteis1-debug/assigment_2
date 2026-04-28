136e3eb649f9cda116f9ef2ffa8a43c6bf8b0126
manar <Manarfteis1@gmail.com>
2026-04-28 21:49:06 +0200

    struct State

--- assignment_2.cpp
+++ assignment_2.cpp
@@ -3,4 +3,12 @@ using namespace std;
 
 const int SIZE = 5; 
 const int MAX_FUEL = 20; 
-const int MAX_STATES = 20000; 
+const int MAX_STATES = 20000; //اقصى حد للحالات 
+
+struct State {   
+    int x , y; 
+    int fuel;
+    bool c1 , c2 , c3 , c4;
+    int g, h, f;
+    int parent; // يخزن كل حالة من وين جت 
+};
